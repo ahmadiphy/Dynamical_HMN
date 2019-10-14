1aed6e73e157d8f18c8508365222bf518fc75efb
Abolfazl Ahmadi-Rahmat <38430894+ahmadiphy@users.noreply.github.com>
2019-10-14 10:47:31 +0330

    Add files via upload

---
+++ dynamical_hmn.cpp
@@ -0,0 +1,167 @@
+#include "dynamical_hmn.h"
+using namespace std;
+
+dynamical_HMN::dynamical_HMN(int inm0,int inl)
+{
+    m0=inm0;
+    l=inl;
+    ll=pow(2,l);
+    outll=ll;
+    outm0=m0;
+    iniLinks=2;
+    for(int i=0;i<l;++i)
+    {
+        Links.push_back(iniLinks);
+        PossibleLinks.push_back(pow(4,i)*pow(m0,2));
+    }
+    Links.shrink_to_fit();
+    PossibleLinks.shrink_to_fit();
+}
+
+void dynamical_HMN::Cal_inverseM(iMatrix &aa)
+{
+
+}
+
+/*
+void dynamical_HMN::Cal_modules_links(iMatrix& aa)
+{
+    int modulB,modulE;
+    for(int i=0;i<ll;++i)
+    {
+        modulB=i*m0;
+        modulE=(i+1)*m0;
+        Links[i]=0;
+        for(int j=modulB;j<modulE;++j)
+        {
+            Links[i]+=aa[j].size();
+        }
+        PossibleLinks[i]=m0-1-Links[i];
+
+    }
+}
+*/
+
+void dynamical_HMN::intraModule(iMatrix& aa)
+{
+    int nn=m0;
+    int nl=0;
+    int modulB,modulE;
+    for(int i=0;i<ll;++i)
+    {
+        modulB=i*nn;
+        modulE=(i+1)*nn;
+        for(int ii=0;ii<nn;++ii)
+        {
+            for(int j=modulB;j<nl;++j)
+            {
+                aa[nl].push_back(j);
+            }
+            for(int j=nl+1;j<modulE;++j)
+            {
+                aa[nl].push_back(j);
+            }
+            nl++;
+        }
+
+    }
+}
+
+void dynamical_HMN::intraModuleLine(iMatrix& aa)
+{
+    int modulB,modulE;
+    for(int i=0;i<ll;++i)
+    {
+        modulB=i*m0;
+        modulE=(i+1)*m0;
+        for(int j=modulB;j<modulE-1;++j)
+        {
+            aa[j].push_back(j+1);
+            aa[j+1].push_back(j);
+        }
+
+    }
+}
+
+void dynamical_HMN::intraModuleRing(iMatrix& aa)
+{
+    int modulB,modulE;
+    for(int i=0;i<ll;++i)
+    {
+        modulB=i*m0;
+        modulE=(i+1)*m0;
+        for(int j=modulB;j<modulE;++j)
+        {
+            aa[j].push_back(j+1);
+            aa[j+1].push_back(j);
+        }
+
+    }
+}
+
+void dynamical_HMN::interModule(iMatrix &aa)
+{
+    int nn=m0;
+    int alpha=iniLinks;
+    random_device rd;
+    mt19937 gen(rd());  // to seed mersenne twister.
+    uniform_real_distribution<> dist(0,1);
+    int ip=1;
+    for(int ii=0;ii<l;ii++)
+    {
+        int il=ip;
+        ip=2*ip;
+        for(int i=0;i<ll-il;i+=ip)
+        {
+            int dis1B=0,dis1E=0;
+            dis1B=i*nn;
+            dis1E=((i+il)*nn)-1;
+            uniform_int_distribution<> dis1(dis1B, dis1E);
+            int dis2B=0,dis2E=0;
+            dis2B=(i+il)*nn;
+            dis2E=((i+(2*il))*nn)-1;
+            uniform_int_distribution<> dis2(dis2B, dis2E);
+            int tch=0;
+            while(tch<alpha){
+                int r1=dis1(gen);
+                int r2=dis2(gen);
+                int rcheck=0;
+                for(int ri=0;ri<aa[r1].size();++ri)
+                {
+                    if(aa[r1][ri]==r2)
+                        rcheck=1;
+                }
+                if(rcheck==0)
+                {
+                    aa[r1].push_back(r2);
+                    aa[r2].push_back(r1);
+                    tch++;
+                }
+            }
+        }
+
+    }
+}
+
+void dynamical_HMN::uniformHIMC(iMatrix &aa, int iLink)
+{
+    random_device rd;//random device to randomize tha seed
+    mt19937 gen(rd());  // to seed mersenne twister.
+    Cal_inverseM(aa);
+    for(int i=0;i<ll;++i)
+    {
+        if(PossibleLinks[i]!=0)
+        {
+            int modulB,modulE;
+            modulB=i*m0;
+            modulE=(i+1)*m0-1;
+
+            uniform_int_distribution<> dis1(modulB, modulE);
+        }
+    }
+}
+
+void dynamical_HMN::uniformNHIMC(iMatrix &aa, int iLink)
+{
+
+}
---
+++ dynamical_hmn.h
@@ -0,0 +1,26 @@
+#include "in.h"
+#ifndef DYNAMICAL_HMN_H
+#define DYNAMICAL_HMN_H
+
+
+class dynamical_HMN
+{
+public:
+    dynamical_HMN(int inm0,int inl);
+    int outll,outm0;//to print number of modules and nodules sizes
+    void intraModule(iMatrix& aa);//building hierarchical modules
+    void intraModuleLine(iMatrix& aa);//building linear modules
+    void intraModuleRing(iMatrix& aa);//building ring modules
+    void uniformHIMC(iMatrix& aa, int iLink);//uniform and hemogenius increasing modular connectivity
+    void uniformNHIMC(iMatrix& aa, int iLink);//uniform and non hemogenius increasing modular connectivity
+    void interModule(iMatrix &aa);//connecting modules together in hierarchical structure
+private:
+    int m0,l,ll,iniLinks;//initial conditions
+    std::vector<int> Links;
+    std::vector<int> PossibleLinks;
+    iMatrix inverseM(ll*m0,iRow());
+    void Cal_inverseM(iMatrix& aa);
+    void Cal_modules_links(iMatrix& aa);
+};
+
+#endif // DYNAMICAL_HMN_H
